ac8ccc6044316b7c235a966ce5cb93922d83703f
Jelle Maas <typiqally@gmail.com>
2022-04-14 15:02:27 +0200

    Add code comments to explain functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import client from "./client/AxiosClient.js"
 import 'dotenv/config'
 
+// Get Canvas course id from the .env file
 const courseId = process.env.CANVAS_COURSE_ID
 console.log(`Targeting course: ${courseId}`)
 
+// Fetch outcome results for the targeted course, this will return all the graded outcomes of the course
 client.get(`v1/courses/${courseId}/outcome_results?per_page=500`)
     .then(function (response) {
         console.log(response.data.outcome_results)
