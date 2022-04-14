a4fe56d409646a808e11b1e953e019bffed1be7c
Jelle Maas <typiqally@gmail.com>
2022-04-14 16:27:54 +0200

    Combine outcomes and assignments and log output to console

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import client from "./client/AxiosClient.js"
 import 'dotenv/config'
 
-const getOutcomesFromResults = async (results) => {
+const getOutcomesAndAssignmentsFromResults = async (results) => {
     let outcomes = {}
+    let assignments = {}
 
     for (const result of results) {
         const outcomeId = result.links.learning_outcome
-        const outcome = outcomes[outcomeId]
+        let outcome = outcomes[outcomeId]
 
         if (outcome === undefined) {
-            outcomes[outcomeId] = await getOutcome(outcomeId)
+            outcome = await getOutcome(outcomeId)
+            outcomes[outcomeId] = outcome
         }
+
+        const assignmentId = result.links.assignment.substring('assignment_'.length)
+        let assignment = assignments[assignmentId]
+
+        if (assignment === undefined) {
+            assignment = await getAssignment(assignmentId)
+            assignments[assignmentId] = assignment
+        }
+
+        if (outcome["assignments"] === undefined) {
+            outcome["assignments"] = []
+        }
+
+        outcome["assignments"].push(assignment)
+
+        if (assignment["outcomes"] === undefined) {
+            assignment["outcomes"] = []
+        }
+
+        assignment["outcomes"].push(outcome)
     }
 
-    return outcomes
+    return {
+        outcomes: outcomes, assignments: assignments
+    }
 }
 
 const getOutcome = async (id) => {
@@ -28,21 +52,6 @@ const getOutcome = async (id) => {
         })
 }
 
-const getAssignmentsFromResults = async (results) => {
-    let assignments = {}
-
-    for (const result of results) {
-        const assignmentId = result.links.assignment.substring('assignment_'.length)
-        const assignment = assignments[assignmentId]
-
-        if (assignment === undefined) {
-            assignments[assignmentId] = await getAssignment(assignmentId)
-        }
-    }
-
-    return assignments
-}
-
 const getAssignment = async (id) => {
     process.stdout.write(`Fetching assignment ${id}... `)
 
@@ -55,6 +64,31 @@ const getAssignment = async (id) => {
         })
 }
 
+const logOutcomes = (outcomes) => {
+    Object.values(outcomes).forEach(outcome => {
+        console.log(`Outcome: ${outcome.title}`)
+
+        outcome.assignments.forEach(assignment => {
+            console.log(`- ${assignment.name}`)
+        })
+
+        console.log('\n===========================================\n')
+    })
+}
+
+const logAssignments = (assignments) => {
+    Object.values(assignments).forEach(assignment => {
+        console.log(`Assignment ${assignment.id}, link: ${assignment.html_url}`)
+        console.log(`Name = ${assignment.name}\n`)
+
+        assignment.outcomes.forEach(outcome => {
+            console.log(`- ${outcome.title}`)
+        })
+
+        console.log('\n===========================================\n')
+    })
+}
+
 // Get Canvas course id from the .env file
 const courseId = process.env.CANVAS_COURSE_ID
 console.log(`Targeting course: ${courseId}`)
@@ -63,9 +97,10 @@ console.log(`Targeting course: ${courseId}`)
 client.get(`v1/courses/${courseId}/outcome_results?per_page=500`)
     .then(async (response) => {
         const data = response.data
-        const outcomes = await getOutcomesFromResults(data.outcome_results)
-        const assignments = await getAssignmentsFromResults(data.outcome_results)
+        const outcomeResults = data.outcome_results.filter(result => result.mastery)
 
-        console.log(outcomes)
-        console.log(assignments)
+        const result = await getOutcomesAndAssignmentsFromResults(outcomeResults)
+
+        logOutcomes(result.outcomes)
+        logAssignments(result.assignments)
     })
