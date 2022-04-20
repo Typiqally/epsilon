f7c65a72318a7c4388de899a39e29ea99158589f
Jelle Maas <typiqally@gmail.com>
2022-04-20 14:06:16 +0200

    Add Axios base URL to dotenv config file

--- src/repository/client/AxiosClient.js
+++ src/repository/client/AxiosClient.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import 'dotenv/config'
 
+const baseUrl = process.env.CANVAS_API_BASE_URL
 const token = process.env.CANVAS_TOKEN
 
-axios.defaults.baseURL = 'https://fhict.instructure.com/api/'
+axios.defaults.baseURL = baseUrl
 axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
 
 export default axios;
