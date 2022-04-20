57c19de25166d321174eb8ccdbed0a9a21062bca
Jelle Maas <typiqally@gmail.com>
2022-04-20 13:08:12 +0200

    Remove comments from log and cleanup code

--- src/CanvasApi.js
+++ src/CanvasApi.js
@@ -7,21 +7,22 @@ let CanvasApi = class {
         process.stdout.write(`Fetching outcome ${id}... `)
 
         return this.client.get(`v1/outcomes/${id}`)
-            .then((response) => {
+            .then(response => {
                 const data = response.data
-                // console.log('\x1b[32m%s\x1b[0m', `done ${data.title}`)
+                console.log('\x1b[32m%s\x1b[0m', `done ${data.title}`)
 
-                return data
-            }).catch((response => console.log(response)))
+                return response.data
+            })
+            .catch(response => console.log(response))
     }
 
     getAssignment = async (courseId, id) => {
         process.stdout.write(`Fetching assignment ${id}... `)
 
         return this.client.get(`v1/courses/${courseId}/assignments/${id}`)
-            .then((response) => {
+            .then(response => {
                 const data = response.data
-                // console.log('\x1b[32m%s\x1b[0m', `done ${data.name}`)
+                console.log('\x1b[32m%s\x1b[0m', `done ${data.name}`)
 
                 return data
             })
@@ -29,21 +30,21 @@ let CanvasApi = class {
 
     getMasteredOutcomes = async (courseId) => {
         return await client.get(`v1/courses/${courseId}/outcome_results?per_page=69420`)
-            .then(async (response) => {
+            .then(async response => {
                 return response.data.outcome_results.filter(result => result.mastery)
             })
     }
 
     getModules = async (courseId) => {
         return await client.get(`v1/courses/${courseId}/modules`)
-            .then(async (response) => {
+            .then(async response => {
                 return response.data
             })
     }
 
     getModuleItems = async (courseId, moduleId) => {
         return await client.get(`v1/courses/${courseId}/modules/${moduleId}/items`)
-            .then(async (response) => {
+            .then(async response => {
                 return response.data
             })
     }
