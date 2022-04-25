4a11e4c997118cf0c7d902a9c32caca6f2e76028
Jelle Maas <typiqally@gmail.com>
2022-04-25 14:18:08 +0200

    Configure and implement application configuration

--- Epsilon.Cli/Program.cs
+++ Epsilon.Cli/Program.cs
@@ -1,9 +1,15 @@
 ﻿using Epsilon.Cli;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration(static config =>
+        {
+            config.SetBasePath(Directory.GetCurrentDirectory());
+            config.AddJsonFile("appsettings.json");
+        })
         .ConfigureServices(static (_, services) => { services.AddHostedService<Startup>(); });
 
 await CreateHostBuilder(args)
--- Epsilon.Cli/Startup.cs
+++ Epsilon.Cli/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Epsilon.Cli;
@@ -6,15 +7,19 @@ namespace Epsilon.Cli;
 public class Startup : IHostedService
 {
     private readonly ILogger<Startup> _logger;
-    
-    public Startup(ILogger<Startup> logger)
+    private readonly IConfiguration _configuration;
+
+    public Startup(ILogger<Startup> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Starting Epsilon");
+        var canvasConfiguration = _configuration.GetRequiredSection("Canvas");
+        var canvasCourseId = canvasConfiguration.GetValue<int>("CourseId");
+        _logger.LogInformation("Starting Epsilon, targeting course: {courseId}", canvasCourseId);
 
         return Task.FromResult(0);
     }
