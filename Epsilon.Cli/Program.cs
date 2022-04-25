36727699269db5428188384081c24e6b9dab1d80
Jelle Maas <typiqally@gmail.com>
2022-04-25 14:06:38 +0200

    Implement dependency injection with hosted service for startup

--- Epsilon.Cli/Program.cs
+++ Epsilon.Cli/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Epsilon.Cli;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices(static (_, services) => { services.AddHostedService<Startup>(); });
+
+await CreateHostBuilder(args)
+    .Build()
+    .RunAsync();
---
+++ Epsilon.Cli/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Epsilon.Cli;
+
+public class Startup : IHostedService
+{
+    private readonly ILogger<Startup> _logger;
+    
+    public Startup(ILogger<Startup> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Starting Epsilon");
+
+        return Task.FromResult(0);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(0);
+    }
+}
