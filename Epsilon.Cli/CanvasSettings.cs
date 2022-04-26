b03d710e12ca9dcb342bdd21069591a0b7ba8d32
Jelle Maas <typiqally@gmail.com>
2022-04-26 16:37:43 +0200

    Specify defaults for each setting

--- Epsilon.Cli/CanvasSettings.cs
+++ Epsilon.Cli/CanvasSettings.cs
@@ -2,7 +2,7 @@
 
 public record CanvasSettings
 {
-    public Uri? ApiUrl { get; set; }
-    public int? CourseId { get; set; }
-    public string? AccessToken { get; set; }
+    public Uri ApiUrl { get; set; } = new("https://fhict.instructure.com/api/");
+    public int CourseId { get; set; }
+    public string AccessToken { get; set; } = string.Empty;
 }
