1e8c9b4e04fbe90cfad1fd3cd7fee53d197cbfec
Jelle Maas <typiqally@gmail.com>
2022-04-26 14:21:44 +0200

    Add abstract class for HTTP services

---
+++ Epsilon.Http.Abstractions/HttpService.cs
@@ -0,0 +1,8 @@
+﻿namespace Epsilon.Http.Abstractions;
+
+public abstract class HttpService
+{
+    protected HttpService(HttpClient client) => Client = client;
+
+    protected HttpClient Client { get; }
+}
