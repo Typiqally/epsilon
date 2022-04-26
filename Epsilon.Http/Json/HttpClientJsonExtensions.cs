8ed47bfaff01ae76e8c7b40fdf2e9d90ee12b4cc
Jelle Maas <typiqally@gmail.com>
2022-04-26 14:23:16 +0200

    Add JSON extensions for the default HTTP API

---
+++ Epsilon.Http/Json/HttpClientJsonExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace Epsilon.Http.Json;
+
+public static class HttpClientJsonExtensions
+{
+    public static Tuple<HttpResponseMessage, T?> Send<T>(
+        this HttpClient client,
+        HttpRequestMessage request,
+        CancellationToken cancellationToken = default)
+    {
+        var response = client.Send(request, cancellationToken);
+        var data =  response.Deserialize<T>();
+
+        return new Tuple<HttpResponseMessage, T?>(response, data);
+    }
+
+    public static Tuple<HttpResponseMessage, T?> Send<T>(
+        this HttpClient client,
+        HttpMethod httpMethod,
+        string requestUri,
+        CancellationToken cancellationToken = default)
+    {
+        var request = new HttpRequestMessage(httpMethod, requestUri);
+
+        return client.Send<T>(request, cancellationToken);
+    }
+
+    public static async Task<Tuple<HttpResponseMessage, T?>> SendAsync<T>(
+        this HttpClient client,
+        HttpRequestMessage request,
+        CancellationToken cancellationToken = default)
+    {
+        var response = await client.SendAsync(request, cancellationToken);
+        var data = await response.DeserializeAsync<T>();
+
+        return new Tuple<HttpResponseMessage, T?>(response, data);
+    }
+
+    public static async Task<Tuple<HttpResponseMessage, T?>> SendAsync<T>(
+        this HttpClient client,
+        HttpMethod httpMethod,
+        string requestUri,
+        CancellationToken cancellationToken = default)
+    {
+        var request = new HttpRequestMessage(httpMethod, requestUri);
+
+        return await client.SendAsync<T>(request, cancellationToken);
+    }
+}
