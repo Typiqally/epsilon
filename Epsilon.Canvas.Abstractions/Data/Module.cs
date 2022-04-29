996a5d60f5627415fe6441307a599d5ffa0e0ce1
Jelle Maas <typiqally@gmail.com>
2022-04-29 13:31:40 +0200

    Add assignments relation to modules

--- Epsilon.Canvas.Abstractions/Data/Module.cs
+++ Epsilon.Canvas.Abstractions/Data/Module.cs
@@ -1,9 +1,13 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
 
 namespace Epsilon.Canvas.Abstractions.Data;
 
 public record Module(
     [property: JsonPropertyName("id")] int Id,
     [property: JsonPropertyName("name")] string Name,
-    [property: JsonPropertyName("items_count")] int Count
-);
+    [property: JsonPropertyName("items_count")] int Count)
+{
+    [JsonIgnore]
+    public IList<Assignment> Assignments { get; set; } = new Collection<Assignment>();
+}
