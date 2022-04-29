68785dc2447a344ff299c11b8e086c0fa342d9c9
Jelle Maas <typiqally@gmail.com>
2022-04-29 14:18:29 +0200

    Add Serilog and update appsettings.example.json

--- Epsilon.Cli/Program.cs
+++ Epsilon.Cli/Program.cs
@@ -3,9 +3,11 @@ using Epsilon.Cli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
-IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
+IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
         .ConfigureAppConfiguration(static config =>
         {
             config.SetBasePath(Directory.GetCurrentDirectory());
@@ -13,10 +15,16 @@ IHostBuilder CreateHostBuilder(string[] args) =>
         })
         .ConfigureServices(static (context, services) =>
         {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(context.Configuration)
+                .CreateLogger();
+
             services.AddCanvas(context.Configuration.GetSection("Canvas"));
             services.AddHostedService<Startup>();
         });
+}
 
 await CreateHostBuilder(args)
+    .UseSerilog()
     .Build()
     .RunAsync();
