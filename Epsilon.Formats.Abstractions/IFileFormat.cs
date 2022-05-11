0a63fbe70a6e7733ad9d211c78113994bc31a052
Neal Geilen <info@nealgeilen.nl>
2022-05-11 19:51:47 +0200

    Added support for file format exports and added CSV file support

---
+++ Epsilon.Formats.Abstractions/IFileFormat.cs
@@ -0,0 +1,9 @@
+﻿using Epsilon.Canvas.Abstractions.Data;
+
+namespace Epsilon.Formats.Abstractions;
+
+public interface IFileFormat
+{
+    IFileFormat FormatFile(IEnumerable<Module> modules);
+    bool CreateDocument(string fileName);
+}
