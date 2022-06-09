3692ab59bbe273efa6a99e993b980ef4c17c780d
Jelle Maas <typiqally@gmail.com>
2022-06-09 10:34:03 +0200

    Cleanup code

--- Epsilon/Export/Exporters/ExcelModuleExporter.cs
+++ Epsilon/Export/Exporters/ExcelModuleExporter.cs
@@ -16,9 +16,62 @@ public class ExcelModuleExporter : ICanvasModuleExporter
 
     public IEnumerable<string> Formats { get; } = new[] { "xls" };
 
-    public  List<Outcome> GetAllOutcomesTypes(Module module)
+    public void Export(IEnumerable<Module> modules, string format)
     {
-        List<Outcome> addedOutcomes = new List<Outcome>();
+        var workbook = new Workbook();
+
+        foreach (var module in modules)
+        {
+            if (!module.HasAssignments())
+            {
+                continue;
+            }
+
+            var outcomes = GetAllOutcomesTypes(module);
+            var worksheet = new Worksheet(module.Name);
+
+            //Because reasons @source https://stackoverflow.com/a/8127642 
+            for (var i = 0; i < 100; i++)
+            {
+                worksheet.Cells[i, 0] = new Cell("");
+            }
+
+            //Adding all the outcomes. 
+            for (var index = 0; index < outcomes.Count; index++)
+            {
+                worksheet.Cells[index, 0] = new Cell(outcomes[index].Title);
+            }
+
+            foreach (var assignment in module.Assignments)
+            {
+                foreach (var outcomeResult in assignment.OutcomeResults)
+                {
+                    if (outcomeResult.Outcome == null)
+                    {
+                        continue;
+                    }
+
+                    var row = GetOutcomeRow(outcomes, outcomeResult.Outcome);
+
+                    //Adding assignments to the outcomes 
+                    var cellValue = worksheet.Cells[row, 1].StringValue;
+                    cellValue += (cellValue != "" ? "\n" : "") + assignment.Name + " " + assignment.Url;
+
+                    worksheet.Cells[row, 1] = new Cell(cellValue);
+                }
+            }
+
+            worksheet.Cells.ColumnWidth[0, 0] = 5000;
+            worksheet.Cells.ColumnWidth[0, 1] = 8000;
+            workbook.Worksheets.Add(worksheet);
+        }
+
+        workbook.Save($"{_options.FormattedOutputName}.{format}");
+    }
+
+    private static List<Outcome> GetAllOutcomesTypes(Module module)
+    {
+        var addedOutcomes = new List<Outcome>();
         if (module.HasAssignments())
         {
             foreach (var assignment in module.Assignments)
@@ -33,64 +86,13 @@ public class ExcelModuleExporter : ICanvasModuleExporter
             }
         }
 
-        return addedOutcomes.OrderByDescending(o => o.Title.Length).ToList();
+        return addedOutcomes.OrderByDescending(static o => o.Title.Length).ToList();
     }
 
-    public int GetOutcomeRow(List<Outcome> outcomes, Outcome outcome)
+    private static int GetOutcomeRow(List<Outcome> outcomes, Outcome outcome)
     {
         var result = outcomes.Find(o => o.Title == outcome.Title);
-        if (result != null)
-        {
-            return outcomes.IndexOf(result);
-        }
 
-        return 0;
-    }
-
-    public void Export(IEnumerable<Module> modules, string format)
-    {
-        Workbook workbook = new Workbook();
-        
-        foreach (var module in modules)
-        {
-            if (module.HasAssignments())
-            {
-                List<Outcome> outcomes = GetAllOutcomesTypes(module);
-                Worksheet worksheet = new Worksheet(module.Name);
-                //Because reasons @source https://stackoverflow.com/a/8127642 
-                for(int i = 0;i < 100; i++)
-                    worksheet.Cells[i,0] = new Cell("");
-
-                //Adding all the outcomes. 
-                for (int index = 0; index < outcomes.Count; index++)
-                {
-                    worksheet.Cells[index, 0] = new Cell(outcomes[index].Title);
-                }
-
-                foreach (var assignment in module.Assignments)
-                {
-                    foreach (var outcomeResult in assignment.OutcomeResults)
-                    {
-                        if (outcomeResult.Outcome != null)
-                        {
-                            int row = GetOutcomeRow(outcomes, outcomeResult.Outcome);
-                            
-                            //Adding assignments to the outcomes 
-                            string cellValue = worksheet.Cells[row, 1].StringValue;
-                            cellValue += (cellValue != "" ? "\n": "") +  assignment.Name + " " + assignment.Url  ;
-                            
-                            worksheet.Cells[row, 1] = new Cell(cellValue);
-                        }
-
-                    }
-                }
-
-                worksheet.Cells.ColumnWidth[0, 0] = 5000;
-                worksheet.Cells.ColumnWidth[0, 1] = 8000;
-                workbook.Worksheets.Add(worksheet); 
-                
-            }
-        }
-        workbook.Save($"{_options.FormattedOutputName}.{format}");
+        return result != null ? outcomes.IndexOf(result) : 0;
     }
 }
