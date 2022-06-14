e4a03d205e7ef0b817acb99bd1fd06eecf0a8642
i404788 <404788@student.fontys.nl>
2022-06-14 11:13:52 +0200

    Fix outcomes being capped at 100

--- Epsilon.Canvas/Service/OutcomeService.cs
+++ Epsilon.Canvas/Service/OutcomeService.cs
@@ -4,6 +4,7 @@ using Epsilon.Canvas.Response;
 using Epsilon.Http.Abstractions;
 using Epsilon.Http.Abstractions.Json;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace Epsilon.Canvas.Service;
 
@@ -28,11 +29,23 @@ public class OutcomeService : HttpService, IOutcomeService
 
     public async Task<IEnumerable<OutcomeResult>?> AllResults(int courseId, int count = 1000)
     {
-        var request = new HttpRequestMessage(HttpMethod.Get, $"v1/courses/{courseId}/outcome_results?per_page={count}");
-        var (response, value) = await Client.SendAsync<OutcomeResultResponse>(request);
+	IEnumerable<OutcomeResult>? res = null;
+	var page = 1;
+	do {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v1/courses/{courseId}/outcome_results?per_page={count}&offset={res?.Count() ?? 0}&page={page}");
+            var (response, value) = await Client.SendAsync<OutcomeResultResponse>(request);
+	    var links = LinkHeader.LinksFromHeader(response);
+
+	    _logger.LogDebug("Fetching outcome results from course #{res} #{len} #{links}", JsonSerializer.Serialize(value?.OutcomeResults), value?.OutcomeResults.Count(), links.NextLink);
+	    res = res == null ? value?.OutcomeResults : res.Concat(value.OutcomeResults);
+	    if (links.NextLink == null)
+	        break;
+	    page += 1;
+
+	} while(res.Count() % 100 == 0);
 
         _logger.LogDebug("Fetching outcome results from course #{CourseId}", courseId);
 
-        return value?.OutcomeResults;
+        return res;
     }
 }
