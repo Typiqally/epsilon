250da5caf4e1429e30a4e69cf2ce53137f9caab7
Jelle Maas <typiqally@gmail.com>
2022-06-14 18:04:44 +0200

    Add rubric assessment and rating data models

---
+++ Epsilon.Canvas.Abstractions/Data/RubricRating.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Epsilon.Canvas.Abstractions.Data;
+
+public record RubricRating(
+    [property: JsonPropertyName("points")] double? Points,
+    [property: JsonPropertyName("learning_outcome_id")] int? OutcomeId
+)
+{
+    [JsonIgnore]
+    public Outcome? Outcome { get; set; }
+}
