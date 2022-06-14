af7aa0c26862737299afc93b9182c043aefec75d
Jelle Maas <typiqally@gmail.com>
2022-06-14 18:06:28 +0200

    Remove loggers from canvas services

--- Epsilon.Canvas/Service/OutcomeService.cs
+++ Epsilon.Canvas/Service/OutcomeService.cs
@@ -3,25 +3,19 @@ using Epsilon.Canvas.Abstractions.Services;
 using Epsilon.Canvas.Response;
 using Epsilon.Http.Abstractions;
 using Epsilon.Http.Abstractions.Json;
-using Microsoft.Extensions.Logging;
 
 namespace Epsilon.Canvas.Service;
 
 public class OutcomeService : HttpService, IOutcomeService
 {
-    private readonly ILogger<OutcomeService> _logger;
-
-    public OutcomeService(HttpClient client, ILogger<OutcomeService> logger) : base(client)
+    public OutcomeService(HttpClient client) : base(client)
     {
-        _logger = logger;
     }
 
     public async Task<Outcome?> Find(int id)
     {
         var request = new HttpRequestMessage(HttpMethod.Get, $"v1/outcomes/{id}");
-        var (response, value) = await Client.SendAsync<Outcome>(request);
-
-        _logger.LogDebug("Fetching outcome #{OutcomeId}", id);
+        var (_, value) = await Client.SendAsync<Outcome>(request);
 
         return value;
     }
@@ -29,9 +23,7 @@ public class OutcomeService : HttpService, IOutcomeService
     public async Task<IEnumerable<OutcomeResult>?> AllResults(int courseId, int count = 1000)
     {
         var request = new HttpRequestMessage(HttpMethod.Get, $"v1/courses/{courseId}/outcome_results?per_page={count}");
-        var (response, value) = await Client.SendAsync<OutcomeResultResponse>(request);
-
-        _logger.LogDebug("Fetching outcome results from course #{CourseId}", courseId);
+        var (_, value) = await Client.SendAsync<OutcomeResultResponse>(request);
 
         return value?.OutcomeResults;
     }
