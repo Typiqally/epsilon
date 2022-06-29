e48a65920f6c032f4ac15d094506d8c96d466348
Jelle Maas <typiqally@gmail.com>
2022-06-29 09:25:54 +0200

    Release v1.0.0-beta3 (#32)
    * Change formats type to `string` and split with comma separator
    
    * Use root configuration instead of configuration section
    
    * Fix exceptions not properly displaying in the console
    
    * Add all module item types according to documentation
    
    * Update application demo GIF
    
    * Create code-analysis.yml
    
    * Remove comments and enable additional queries
    
    * Removed push from code-analysis.yml workflow
    
    * Typo code-analysis.yml
    
    Co-authored-by: Neal Geilen <info@nealgeilen.nl>

--- Epsilon.Canvas.Abstractions/Model/ModuleItem.cs
+++ Epsilon.Canvas.Abstractions/Model/ModuleItem.cs
@@ -5,7 +5,6 @@ namespace Epsilon.Canvas.Abstractions.Model;
 public record ModuleItem(
     [property: JsonPropertyName("id")] int Id,
     [property: JsonPropertyName("title")] string Title,
-    [property: JsonPropertyName("type"), JsonConverter(typeof(JsonStringEnumConverter))]
-    ModuleItemType? Type,
+    [property: JsonPropertyName("type")] ModuleItemType? Type,
     [property: JsonPropertyName("content_id")] int? ContentId
 );
