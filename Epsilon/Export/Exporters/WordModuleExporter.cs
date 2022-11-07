772d47157db64405297ce4b1532eee9c3911e3fa
Jelle Maas <typiqally@gmail.com>
2022-11-07 19:59:29 +0100

    Reduce nesting

--- Epsilon/Export/Exporters/WordModuleExporter.cs
+++ Epsilon/Export/Exporters/WordModuleExporter.cs
@@ -21,65 +21,64 @@ public class WordModuleExporter : ICanvasModuleExporter
 
     public void Export(IEnumerable<Module> modules, string format)
     {
-        using (DocX document = DocX.Create($"{_options.FormattedOutputName}.docx"))
+        using var document = DocX.Create($"{_options.FormattedOutputName}.docx");
+        
+        document.AddFooters();
+        var link = document.AddHyperlink("Epsilon", new Uri("https://github.com/Typiqally/epsilon"));
+
+        document.Footers.Odd
+            .InsertParagraph("Created with ")
+            .AppendHyperlink(link).Color(Color.Blue).UnderlineStyle(UnderlineStyle.singleLine);
+
+        foreach (var module in modules.Where(static m => m.Collection.OutcomeResults.Any()))
         {
-            document.AddFooters();
-            var link = document.AddHyperlink("Epsilon", new Uri("https://github.com/Typiqally/epsilon"));
+            var links = module.Collection.Links;
+            var alignments = links.AlignmentsDictionary;
+            var outcomes = links.OutcomesDictionary;
 
-            document.Footers.Odd
-                .InsertParagraph("Created with ")
-                .AppendHyperlink(link).Color(Color.Blue).UnderlineStyle(UnderlineStyle.singleLine);
+            var table = document.AddTable(1, 3);
 
-            foreach (var module in modules.Where(static m => m.Collection.OutcomeResults.Any()))
+            table.Rows[0].Cells[0].Paragraphs[0].Append("KPI");
+            table.Rows[0].Cells[1].Paragraphs[0].Append("Assignment(s)");
+            table.Rows[0].Cells[2].Paragraphs[0].Append("Score");
+
+            foreach (var (outcomeId, outcome) in outcomes)
             {
-                var links = module.Collection.Links;
-                var alignments = links.AlignmentsDictionary;
-                var outcomes = links.OutcomesDictionary;
+                var assignmentIds = module.Collection.OutcomeResults
+                    .Where(o => o.Link.Outcome == outcomeId && o.Grade() != null)
+                    .Select(static o => o.Link.Assignment)
+                    .ToArray();
 
-                var table = document.AddTable(1, 3);
-
-                table.Rows[0].Cells[0].Paragraphs[0].Append("KPI");
-                table.Rows[0].Cells[1].Paragraphs[0].Append("Assignment(s)");
-                table.Rows[0].Cells[2].Paragraphs[0].Append("Score");
-
-                foreach (var (outcomeId, outcome) in outcomes)
+                if (assignmentIds.Any())
                 {
-                    var assignmentIds = module.Collection.OutcomeResults
-                        .Where(o => o.Link.Outcome == outcomeId && o.Grade() != null)
-                        .Select(static o => o.Link.Assignment)
-                        .ToArray();
+                    var row = table.InsertRow();
+                    row.Cells[0].Paragraphs[0].Append(outcome.Title + " " + outcome.ShortDescription());
 
-                    if (assignmentIds.Any())
+                    var cellValueBuilder = new StringBuilder();
+
+                    foreach (var (_, alignment) in alignments.Where(a => assignmentIds.Contains(a.Key)))
                     {
-                        var row = table.InsertRow();
-                        row.Cells[0].Paragraphs[0].Append(outcome.Title + " " + outcome.ShortDescription());
-
-                        var cellValueBuilder = new StringBuilder();
-
-                        foreach (var (alignmentId, alignment) in alignments.Where(a => assignmentIds.Contains(a.Key)))
-                        {
-                            cellValueBuilder.AppendLine($"{alignment.Name} {alignment.Url}");
-                        }
-
-                        row.Cells[1].Paragraphs[0].Append(cellValueBuilder.ToString());
-
-                        var cellValueOutComeResultsBuilder = new StringBuilder();
-                        foreach (var outcomeResult in module.Collection.OutcomeResults.Where(result =>
-                                     result.Link.Outcome == outcomeId))
-                        {
-                            cellValueOutComeResultsBuilder.AppendLine(outcomeResult.Grade());
-                        }
-
-                        row.Cells[2].Paragraphs[0].Append(cellValueOutComeResultsBuilder.ToString());
+                        cellValueBuilder.AppendLine($"{alignment.Name} {alignment.Url}");
                     }
-                }
 
-                var par = document.InsertParagraph(module.Name);
-                par.FontSize(24);
-                par.InsertTableAfterSelf(table).InsertPageBreakAfterSelf();
+                    row.Cells[1].Paragraphs[0].Append(cellValueBuilder.ToString());
+
+                    var cellValueOutComeResultsBuilder = new StringBuilder();
+                    foreach (var outcomeResult in module.Collection.OutcomeResults.Where(result =>
+                                 result.Link.Outcome == outcomeId))
+                    {
+                        cellValueOutComeResultsBuilder.AppendLine(outcomeResult.Grade());
+                    }
+
+                    row.Cells[2].Paragraphs[0].Append(cellValueOutComeResultsBuilder.ToString());
+                }
             }
 
-            document.Save();
+            var par = document.InsertParagraph(module.Name);
+            par.FontSize(24);
+            par.InsertTableAfterSelf(table).InsertPageBreakAfterSelf();
         }
+
+        document.Save();
     }
 }
