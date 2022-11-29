27ab00567d5f0c6d4dbbe9bc6edf47bd8c670a65
Neal Geilen <info@nealgeilen.nl>
2022-11-29 11:57:26 +0100

    POC Open XML SDK

--- Epsilon/Export/Exporters/WordModuleExporter.cs
+++ Epsilon/Export/Exporters/WordModuleExporter.cs
@@ -1,11 +1,11 @@
-﻿using System.Diagnostics;
-using System.Drawing;
-using System.Text;
+﻿using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+using DocumentFormat.OpenXml.Wordprocessing;
 using Epsilon.Abstractions.Export;
 using Epsilon.Canvas.Abstractions.Model;
 using Microsoft.Extensions.Options;
-using Xceed.Document.NET;
-using Xceed.Words.NET;
 
 namespace Epsilon.Export.Exporters;
 
@@ -22,67 +22,166 @@ public class WordModuleExporter : ICanvasModuleExporter
 
     public async Task Export(IAsyncEnumerable<ModuleOutcomeResultCollection> data, string format)
     {
-        using var document = DocX.Create($"{_options.FormattedOutputName}.docx");
-        
-        document.AddFooters();
-        var link = document.AddHyperlink(Constants.ProjectName, Constants.RepositoryUri);
-
-        document.Footers.Odd
-            .InsertParagraph("Created with ")
-            .AppendHyperlink(link).Color(Color.Blue).UnderlineStyle(UnderlineStyle.singleLine);
-
-        await foreach (var item in data.Where(static m => m.Collection.OutcomeResults.Any()))
+        string fileName = $"{_options.FormattedOutputName}.docx";
+        using (var document = WordprocessingDocument.Create(fileName,
+                   WordprocessingDocumentType.Document))
         {
-            var links = item.Collection.Links;
-            
-            Debug.Assert(links != null, nameof(links) + " != null");
-            
-            var alignments = links.AlignmentsDictionary;
-            var outcomes = links.OutcomesDictionary;
+            document.AddMainDocumentPart();
+            document.MainDocumentPart!.Document = new Document(new Body());
+            var doc = document.MainDocumentPart.Document;
+            // Add a main document part. 
+            // MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
 
-            var table = document.AddTable(1, 3);
+            // Create the document structure and add some text.
+            // mainPart.Document = new Document();
+            // Body body = mainPart.Document.AppendChild(new Body());
+            // Paragraph para = body.AppendChild(new Paragraph());
+            // Run run = para.AppendChild(new Run());
+            // run.AppendChild(new Text("Create text in body - CreateWordprocessingDocument"));
 
-            table.Rows[0].Cells[0].Paragraphs[0].Append("KPI");
-            table.Rows[0].Cells[1].Paragraphs[0].Append("Assignment(s)");
-            table.Rows[0].Cells[2].Paragraphs[0].Append("Score");
 
-            foreach (var (outcomeId, outcome) in outcomes)
+            await foreach (var item in data.Where(static m => m.Collection.OutcomeResults.Any()))
             {
-                var assignmentIds = item.Collection.OutcomeResults
-                    .Where(o => o.Link.Outcome == outcomeId && o.Grade() != null)
-                    .Select(static o => o.Link.Assignment)
-                    .ToArray();
+                var links = item.Collection.Links;
+                var alignments = links.AlignmentsDictionary;
+                var outcomes = links.OutcomesDictionary;
 
-                if (assignmentIds.Any())
-                {
-                    var row = table.InsertRow();
-                    row.Cells[0].Paragraphs[0].Append(outcome.Title + " " + outcome.ShortDescription());
+                Table table = new Table();
+                TableRow headerRow = new TableRow();
+                var kpiCell = new TableCell();
+                kpiCell.Append(new Paragraph(new Run(new Text("KPI"))));
+                var assignmentCell = new TableCell();
+                assignmentCell.Append(new Paragraph(new Run(new Text("Assignment(s)"))));
+                var scoreCell = new TableCell();
+                scoreCell.Append(new Paragraph(new Run(new Text("Scores"))));
+                headerRow.Append(kpiCell);
+                headerRow.Append(assignmentCell);
+                headerRow.Append(scoreCell);
+                
+                table.Append(headerRow);
 
-                    var cellValueBuilder = new StringBuilder();
+                // foreach (var (outcomeId, outcome) in outcomes)
+                // {
+                //     var assignmentIds = item.Collection.OutcomeResults
+                //         .Where(o => o.Link.Outcome == outcomeId && o.Grade() != null)
+                //         .Select(static o => o.Link.Assignment)
+                //         .ToArray();
 
-                    foreach (var (_, alignment) in alignments.Where(a => assignmentIds.Contains(a.Key)))
-                    {
-                        cellValueBuilder.AppendLine($"{alignment.Name} {alignment.Url}");
-                    }
+                // if (assignmentIds.Any())
+                // {
+                //     TableRow row = new TableRow();
+                //     
+                //     var outcomeCell = new TableCell();
+                //     outcomeCell.Append(new Text($"{outcome.Title} {outcome.ShortDescription()}"));
+                //     
+                //     row.Append(new TableCell(outcomeCell));
+                //
+                //     var cellValueBuilder = new StringBuilder();
+                //
+                //     foreach (var (_, alignment) in alignments.Where(a => assignmentIds.Contains(a.Key)))
+                //     {
+                //         cellValueBuilder.AppendLine($"{alignment.Name} {alignment.Url}");
+                //     }
+                //
+                //     var assignmentXCell = new TableCell();
+                //     assignmentXCell.Append(new Text(cellValueBuilder.ToString()));
+                //     
+                //     row.Append(new TableCell(assignmentXCell));
+                //
+                //     var cellValueOutComeResultsBuilder = new StringBuilder();
+                //     foreach (var outcomeResult in item.Collection.OutcomeResults.Where(result =>
+                //                  result.Link.Outcome == outcomeId))
+                //     {
+                //         cellValueOutComeResultsBuilder.AppendLine(outcomeResult.Grade());
+                //     }
+                //     
+                //     var scoresCell = new TableCell();
+                //     scoresCell.Append(new Text(cellValueBuilder.ToString()));
+                //     
+                //     row.Append(scoresCell);
+                // }
+                // }
 
-                    row.Cells[1].Paragraphs[0].Append(cellValueBuilder.ToString());
-
-                    var cellValueOutComeResultsBuilder = new StringBuilder();
-                    foreach (var outcomeResult in item.Collection.OutcomeResults.Where(result =>
-                                 result.Link.Outcome == outcomeId))
-                    {
-                        cellValueOutComeResultsBuilder.AppendLine(outcomeResult.Grade());
-                    }
-
-                    row.Cells[2].Paragraphs[0].Append(cellValueOutComeResultsBuilder.ToString());
-                }
+                doc?.Body?.Append(table);
             }
 
-            var par = document.InsertParagraph(item.Module.Name);
-            par.FontSize(24);
-            par.InsertTableAfterSelf(table).InsertPageBreakAfterSelf();
+            document?.Save();
+            document?.Close();
         }
 
-        document.Save();
+        ValidateWordDocument(fileName);
+        ValidateCorruptedWordDocument(fileName);
+    }
+
+    public static void ValidateWordDocument(string filepath)
+    {
+        using (WordprocessingDocument wordprocessingDocument =
+               WordprocessingDocument.Open(filepath, true))
+        {
+            try
+            {
+                OpenXmlValidator validator = new OpenXmlValidator();
+                int count = 0;
+                foreach (ValidationErrorInfo error in
+                         validator.Validate(wordprocessingDocument))
+                {
+                    count++;
+                    Console.WriteLine("Error " + count);
+                    Console.WriteLine("Description: " + error.Description);
+                    Console.WriteLine("ErrorType: " + error.ErrorType);
+                    Console.WriteLine("Node: " + error.Node);
+                    Console.WriteLine("Path: " + error.Path.XPath);
+                    Console.WriteLine("Part: " + error.Part.Uri);
+                    Console.WriteLine("-------------------------------------------");
+                }
+
+                Console.WriteLine("count={0}", count);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            wordprocessingDocument.Close();
+        }
+    }
+
+    public static void ValidateCorruptedWordDocument(string filepath)
+    {
+        // Insert some text into the body, this would cause Schema Error
+        using (WordprocessingDocument wordprocessingDocument =
+               WordprocessingDocument.Open(filepath, true))
+        {
+            // Insert some text into the body, this would cause Schema Error
+            // Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+            // Run run = new Run(new Text("some text"));
+            // body.Append(run);
+
+            try
+            {
+                OpenXmlValidator validator = new OpenXmlValidator();
+                int count = 0;
+                foreach (ValidationErrorInfo error in
+                         validator.Validate(wordprocessingDocument))
+                {
+                    count++;
+                    Console.WriteLine("Error " + count);
+                    Console.WriteLine("Description: " + error.Description);
+                    Console.WriteLine("ErrorType: " + error.ErrorType);
+                    Console.WriteLine("Node: " + error.Node);
+                    Console.WriteLine("Path: " + error.Path.XPath);
+                    Console.WriteLine("Part: " + error.Part.Uri);
+                    Console.WriteLine("-------------------------------------------");
+                }
+
+                Console.WriteLine("count={0}", count);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
