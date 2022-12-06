f9cdac219fdb6ca78ed2f23b2ce4465ca6da9662
Neal Geilen <info@nealgeilen.nl>
2022-12-06 13:46:04 +0100

    POC

--- Epsilon/Export/ExportOptions.cs
+++ Epsilon/Export/ExportOptions.cs
@@ -5,6 +5,7 @@ public class ExportOptions
     public string OutputName { get; set; } = $"{Constants.ProjectName}-Export-{{DateTime}}";
 
     public string Formats { get; set; } = "console";
+    public string Modules { get; set; } = "";
 
     public string FormattedOutputName => OutputName
         .Replace("{DateTime}", DateTime.Now.ToString("ddMMyyyyHHmmss"));
