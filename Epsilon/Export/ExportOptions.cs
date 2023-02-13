fc71e759dbec8f1bef9568d030a735a24ff4dd3f
Jelle Maas <typiqally@gmail.com>
2023-02-13 12:30:21 +0100

    Fix module filter not returning empty list when option is null

--- Epsilon/Export/ExportOptions.cs
+++ Epsilon/Export/ExportOptions.cs
@@ -5,8 +5,8 @@ public class ExportOptions
     public string OutputName { get; set; } = $"{Constants.ProjectName}-Export-{{DateTime}}";
 
     public string Formats { get; set; } = "console";
-    
-    public string Modules { get; set; } = "";
+
+    public string? Modules { get; set; } = null;
 
     public string FormattedOutputName => OutputName
         .Replace("{DateTime}", DateTime.Now.ToString("ddMMyyyyHHmmss"));
