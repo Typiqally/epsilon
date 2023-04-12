87df124bb52be8357a72851c30038db5eac24945
Jelle Maas <typiqally@gmail.com>
2023-04-12 12:04:09 +0200

    Remove obsolete styling

--- Epsilon.Host.Frontend/src/App.vue
+++ Epsilon.Host.Frontend/src/App.vue
@@ -1,22 +1,8 @@
 <template>
-    <KpiMatrix></KpiMatrix>
-    <KpiTable></KpiTable>
+  <KpiMatrix />
+  <KpiTable />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
 <script setup lang="ts">
 import KpiTable from "./components/Competance/KpiTable.vue";
 import KpiMatrix from "./components/Competance/KpiMatrix.vue";
