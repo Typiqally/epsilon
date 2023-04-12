38234a6f93ba285a0a4f9cef8bfc902fa2d8f494
Jelle Maas <typiqally@gmail.com>
2023-04-12 12:09:15 +0200

    Add Vue router

--- Epsilon.Host.Frontend/src/App.vue
+++ Epsilon.Host.Frontend/src/App.vue
@@ -1,9 +1,3 @@
 <template>
-  <KpiMatrix />
-  <KpiTable />
+  <router-view/>
 </template>
-
-<script setup lang="ts">
-import KpiTable from "./components/Competance/KpiTable.vue";
-import KpiMatrix from "./components/Competance/KpiMatrix.vue";
-</script>
---
+++ Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <KpiMatrix />
+  <KpiTable />
+</template>
+
+<script>
+import KpiMatrix from "@/components/KpiMatrix.vue";
+import KpiTable from "@/components/KpiTable.vue";
+
+export default {
+    name: "PerformanceDashboard",
+    components: {KpiTable, KpiMatrix}
+}
+</script>
+
+<style scoped>
+
+</style>
