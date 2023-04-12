6afaf4684e3e6d2a10529bb916d2ddf33c158646
Jelle Maas <typiqally@gmail.com>
2023-04-12 12:15:59 +0200

    Resolve ESLint warnings

--- Epsilon.Host.Frontend/src/components/KpiTable.vue
+++ Epsilon.Host.Frontend/src/components/KpiTable.vue
@@ -1,58 +1,58 @@
 <template>
-    <table>
-        <thead>
-        <tr>
-            <td></td>
-            <th>Analyse</th>
-            <th>Advise</th>
-            <th>Design</th>
-            <th>Realise</th>
-            <th>Manage & Control</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th>User interaction</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Infrastructure</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Organisational processes</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Software</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Hardware interfacing</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        </tbody>
-    </table>
+  <table>
+    <thead>
+      <tr>
+        <td />
+        <th>Analyse</th>
+        <th>Advise</th>
+        <th>Design</th>
+        <th>Realise</th>
+        <th>Manage & Control</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>User interaction</th>
+        <td />
+        <td />
+        <td />
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <th>Infrastructure</th>
+        <td />
+        <td />
+        <td />
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <th>Organisational processes</th>
+        <td />
+        <td />
+        <td />
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <th>Software</th>
+        <td />
+        <td />
+        <td />
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <th>Hardware interfacing</th>
+        <td />
+        <td />
+        <td />
+        <td />
+        <td />
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
@@ -62,23 +62,28 @@ export default {
 </script>
 
 <style>
-tbody tr th{
+tbody tr th {
     text-align: left;
 }
-tbody tr td{
+
+tbody tr td {
     padding: 8px;
     min-width: 100px;
 }
-tbody tr{
+
+tbody tr {
     border-bottom: 2px solid black;
 }
-tbody td{
+
+tbody td {
     border: 2px solid black;
 }
-table{
+
+table {
     border-collapse: collapse;
 }
-thead th{
+
+thead th {
 
 }
 </style>
