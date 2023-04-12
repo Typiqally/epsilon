d85d415224b11c6668f7e54705cb10272d92328f
Jelle Maas <typiqally@gmail.com>
2023-04-12 12:40:26 +0200

    Add authorization view and controller

--- Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
+++ Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
@@ -1,16 +1,33 @@
 <template>
-  <KpiMatrix />
-  <KpiTable />
+  <KpiMatrix/>
+  <KpiTable/>
 </template>
 
-<script>
+<script setup lang="ts">
+import {onMounted} from "vue";
+
 import KpiMatrix from "@/components/KpiMatrix.vue";
 import KpiTable from "@/components/KpiTable.vue";
+import router from "@/router";
 
-export default {
-    name: "PerformanceDashboard",
-    components: {KpiTable, KpiMatrix}
-}
+onMounted(() => {
+    fetch('https://localhost:7084/component/competence_profile?courseId=1', {credentials: "include"})
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+
+            return Promise.reject(response);
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            if (error.status == 401) {
+                router.push('/auth')
+            }
+        })
+})
 </script>
 
 <style scoped>
