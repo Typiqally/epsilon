9d7c1ecfdd8aae24599c0868d5f80dde526ed220
Jelle Maas <typiqally@gmail.com>
2023-04-12 12:40:26 +0200

    Add authorization view and controller

--- Epsilon.Host.Frontend/src/router/index.ts
+++ Epsilon.Host.Frontend/src/router/index.ts
@@ -1,12 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import PerformanceDashboard from "@/views/PerformanceDashboard.vue";
+import Authorize from "@/views/Authorize.vue";
 
 const routes = [
     {
         path: '/',
         name: 'PerformanceDashboard',
         component: PerformanceDashboard
-    }
+    },
+    {
+        path: '/auth',
+        name: 'Authorize',
+        component: Authorize
+    },
 ]
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
