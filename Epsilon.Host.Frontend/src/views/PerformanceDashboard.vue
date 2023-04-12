cd0133119cd532d062d15f625d1e08f73434f3f1
Neal Geilen <info@nealgeilen.nl>
2023-04-12 15:16:37 +0200

    A lot of stuff

--- Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
+++ Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
@@ -1,17 +1,30 @@
 <template>
-  <KpiMatrix/>
-  <KpiTable/>
+    <div v-if="data !== {}">
+        <KpiMatrix :hbo-i-domain="data.hboIDomain"></KpiMatrix>
+        <KpiTable :hbo-i-domain="data.hboIDomain"></KpiTable>
+        <PersonalDevelopmentMatrix></PersonalDevelopmentMatrix>
+    </div>
 </template>
 
 <script setup lang="ts">
 import {onMounted} from "vue";
-
+import router from "@/router";
+import PersonalDevelopmentMatrix from "@/components/Competance/PersonalDevelopmentMatrix.vue";
 import KpiMatrix from "@/components/KpiMatrix.vue";
 import KpiTable from "@/components/KpiTable.vue";
-import router from "@/router";
+import {Api} from "./../Logic/Api";
+let data = {};
 
 onMounted(() => {
-    fetch('https://localhost:7084/component/competence_profile?courseId=1', {credentials: "include"})
+    const App = new Api()
+
+
+    App.component.competenceProfileMockList()
+        .then((r) => {
+            data = r.data
+        })
+
+    App.component.competenceProfileList()
         .then(response => {
             if (response.ok) {
                 return response.json()
