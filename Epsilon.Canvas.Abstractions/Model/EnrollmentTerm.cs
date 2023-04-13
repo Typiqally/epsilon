b42edff01ef3de4977ee048eaf5b0389b6a7aefd
Tara <tarawillink@gmail.com>
2023-04-13 13:47:04 +0200

    Fetch terms from canvas

---
+++ Epsilon.Canvas.Abstractions/Model/EnrollmentTerm.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Epsilon.Canvas.Abstractions.Model;
+
+public record EnrollmentTerm(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("start_at")] DateTime? StartAt,
+    [property: JsonPropertyName("end_at")] DateTime? EndAt
+);
---
+++ Epsilon.Canvas.Abstractions/Service/IAccountHttpService.cs
@@ -0,0 +1,8 @@
+﻿using Epsilon.Canvas.Abstractions.Model;
+
+namespace Epsilon.Canvas.Abstractions.Service;
+
+public interface IAccountHttpService
+{
+    public Task<IEnumerable<EnrollmentTerm>>? GetAllTerms(int accountId);
+}
