8e49024a5f36b9293c9c38fa7c9e046d7d334ab8
Tara <tarawillink@gmail.com>
2023-04-13 14:40:28 +0200

    Add filter enrolment term function

---
+++ Epsilon/Component/FilterEnrollmentTerms.cs
@@ -0,0 +1,18 @@
+﻿using Epsilon.Abstractions.Model;
+using Epsilon.Canvas.Abstractions.Model;
+using Epsilon.Canvas.Abstractions.Model.GraphQl;
+
+namespace Epsilon.Component;
+
+public class FilterEnrollmentTerms
+{
+    public IEnumerable<EnrollmentTerm> FilterEnrollmentTermsByOutcome(IEnumerable<EnrollmentTerm> terms, IEnumerable<ProfessionalDevelopmentProfileOutcome> outcomes)
+    {
+        var filteredTerms = terms.Where(term => term.StartAt.HasValue)
+            .Where(term => outcomes.Any(outcome =>
+                outcome.AssessedAt > term.StartAt.Value && outcome.AssessedAt < term.EndAt))
+            .Distinct();
+
+        return filteredTerms.OrderBy(term => term.StartAt);
+    }
+}
