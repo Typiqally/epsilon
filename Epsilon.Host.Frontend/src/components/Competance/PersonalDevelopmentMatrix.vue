ffe3f01f88774e2cce3cb430acb9f3e82070106b
Neal Geilen <info@nealgeilen.nl>
2023-04-13 13:25:06 +0200

    Set it all to vue 3 supported format

--- Epsilon.Host.Frontend/src/components/Competance/PersonalDevelopmentMatrix.vue
+++ Epsilon.Host.Frontend/src/components/Competance/PersonalDevelopmentMatrix.vue
@@ -1,92 +1,85 @@
 <template>
-    <apexcharts type="bar" :options="chartOptions" :series="series" v-if="!!hboIDomain"></apexcharts>
+    <apexcharts type="bar" :options="chartOptions" :series="series" v-if="!!domain"></apexcharts>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import apexcharts from "vue3-apexcharts";
+import {HboIDomain, ProfessionalTaskOutcome} from "/@/logic/Api";
+import {watch} from "vue";
 
-export default {
-    name: "PersonalDevelopmentMatrix",
-    components: {apexcharts},
-    props: {
-        hboIDomain: {
-            default: {}
-        }
-    },
-    watch: {
-        hboIDomain() {
-            for (const pd in this.hboIDomain.professionalSkills) {
-                console.log(pd, this.hboIDomain.professionalSkills[pd].name)
-                this.chartOptions.xaxis.categories.push(this.hboIDomain.professionalSkills[pd].name)
-            }
-        }
-    },
-    data() {
-        return {
-            series: [{
-                name: '',
-                data: [44, 55, 41, 67]
-            }
-            ],
-            chartOptions: {
-                annotations: {
-                    yaxis: [
-                        {
-                            y: 40,
-                            borderColor: 'red',
-                            strokeDashArray: 0,
-                            label: {
-                                borderColor: 'red',
-                                style: {
-                                    color: '#fff',
-                                    background: 'red'
-                                },
-                                text: 'Mastery'
-                            }
-                        }
-                    ]
-                },
-                colors: ["#A8D08D"],
-                chart: {
-                    type: 'bar',
-                    stacked: true,
-                    toolbar: {
-                        show: false
+const props = defineProps<{
+    domain: HboIDomain
+    data: ProfessionalTaskOutcome[]
+}>()
+
+const series = [{
+    name: '',
+    data: [44, 55, 41, 67]
+}
+]
+const chartOptions = {
+    annotations: {
+        yaxis: [
+            {
+                y: 40,
+                borderColor: 'red',
+                strokeDashArray: 0,
+                label: {
+                    borderColor: 'red',
+                    style: {
+                        color: '#fff',
+                        background: 'red'
                     },
-                    zoom: {
-                        enabled: false
-                    }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        borderRadius: 4,
-                        dataLabels: {}
-                    },
-                },
-                xaxis: {
-                    type: 'string',
-                    categories: [],
-                },
-                yaxis: {
-                    show: false,
-                },
-                legend: {
-                    position: 'bottom'
-                },
-                fill: {
-                    opacity: 1
-                },
-                tooltip: {
-                    enabled: false
+                    text: 'Mastery'
                 }
-            },
+            }
+        ]
+    },
+    colors: ["#A8D08D"],
+    chart: {
+        type: 'bar',
+        stacked: true,
+        toolbar: {
+            show: false
+        },
+        zoom: {
+            enabled: false
         }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            borderRadius: 4,
+            dataLabels: {}
+        },
+    },
+    xaxis: {
+        type: 'string',
+        categories: [],
+    },
+    yaxis: {
+        show: false,
+    },
+    legend: {
+        position: 'bottom'
+    },
+    fill: {
+        opacity: 1
+    },
+    tooltip: {
+        enabled: false
     }
 }
+
+watch
+(() => props.domain, () => {
+    for (const pd in props.domain.professionalSkills) {
+        chartOptions.xaxis.categories.push(props.domain.professionalSkills?.[pd].name)
+    }
+})
 </script>
 
 <style scoped>
--- Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
+++ Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
@@ -1,53 +1,43 @@
 <template>
-    <div v-if="data">
-        <KpiMatrix :hbo-i-domain="data.hboIDomain"></KpiMatrix>
-        <KpiTable :hbo-i-domain="data.hboIDomain" :data="data.professionalTaskOutcomes"></KpiTable>
-        <PersonalDevelopmentMatrix :hbo-i-domain="data.hboIDomain"></PersonalDevelopmentMatrix>
+    <div v-if="data !== {}">
+        <KpiMatrix :domain="data.hboIDomain"></KpiMatrix>
+        <KpiTable :domain="data.hboIDomain" :data="data.professionalTaskOutcomes"></KpiTable>
+        <PersonalDevelopmentMatrix :domain="data.hboIDomain" :data="data"></PersonalDevelopmentMatrix>
     </div>
 </template>
 
-<script lang="ts">
-import {Api} from "@/logic/Api";
+<script lang="ts" setup>
+import {Api, HttpResponse, CompetenceProfile} from "@/logic/Api";
 import KpiMatrix from "@/components/Competance/KpiMatrix.vue";
 import KpiTable from "@/components/Competance/KpiTable.vue";
 import PersonalDevelopmentMatrix from "@/components/Competance/PersonalDevelopmentMatrix.vue";
-import {CompetenceProfile} from "@/logic/Api";
-import {HttpResponse} from "@/logic/Api";
-let data = {};
+import {onMounted} from "vue";
+let data: CompetenceProfile = {};
+const App = new Api();
 
-export default {
-    name: "PerformanceDashboard",
-    components: {PersonalDevelopmentMatrix, KpiTable, KpiMatrix},
-    data() {
-      return {
-          App: new Api(),
-          data: {}
-      }
-    },
-    mounted() {
-        this.App.component.competenceProfileMockList()
-            .then((r:HttpResponse<any>) => {
-                this.data = r.data as CompetenceProfile
-            })
+onMounted(() => {
+    App.component.competenceProfileMockList()
+        .then((r:HttpResponse<any>) => {
+            data = r.data as CompetenceProfile
+        })
 
-        this.App.component.competenceProfileList()
-        // .then(response => {
-        //     if (response.ok) {
-        //         return response.json()
-        //     }
-        //
-        //     return Promise.reject(response);
-        // })
-        // .then(data => {
-        //     console.log(data)
-        // })
-        // .catch(error => {
-        //     if (error.status == 401) {
-        //         router.push('/auth')
-        //     }
-        // })
-    }
-}
+    App.component.competenceProfileList()
+    // .then(response => {
+    //     if (response.ok) {
+    //         return response.json()
+    //     }
+    //
+    //     return Promise.reject(response);
+    // })
+    // .then(data => {
+    //     console.log(data)
+    // })
+    // .catch(error => {
+    //     if (error.status == 401) {
+    //         router.push('/auth')
+    //     }
+    // })
+})
 </script>
 
 <style scoped>
