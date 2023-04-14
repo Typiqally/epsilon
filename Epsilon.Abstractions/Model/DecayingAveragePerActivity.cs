2e0ee05fcf6b60ad50054890c0d339260a21fd06
jendafojtik <83012368+jendafojtik@users.noreply.github.com>
2023-04-14 16:22:14 +0200

    Add decaying average calculations (#80)
    * initial version of the decaying average functionality
    
    * Added Professional development
    
    * Rename namespace to correspond with coding guidelines
    
    * Separate records into different files
    
    * Rename Value to Name to correspond with domain
    
    * Rename Value to Level to correspond with domain
    
    * Add professional skills to domain
    
    * Add short name to architectural layer and professional skills
    
    * Rename properties in competence profile random data filler
    
    * Rename CompetenceProfileOutcome to ProfessionalTaskOutcome
    
    * Add Professional skill outcome to competence profile
    
    * Use singular name
    
    * Use id references instead of name values
    
    * Use type keyword
    
    * re-adding decaying average files and logic
    
    * Add submissions from all courses from student to competence profile transformation
    
    * Fix merge issues
    
    * Add task and skill outcomes to competence profile return type
    
    * Fetch terms from canvas
    
    * Remove redundant imports
    
    * Change default application url
    
    * Prevent browser from launching on every restart
    
    * Cleanup code
    
    * Add filter enrolment term function
    
    * Add terms to competence profile
    
    * Remove unused terms function
    
    * Added filter on professional outcomes and sorting
    
    * Removed unused things
    
    * V0.1
    
    * Defining colors for elements
    
    * Remove obsolete styling
    
    * Update pnpm lock file
    
    * Add Vue router
    
    * Resolve ESLint warnings
    
    * Add authorization view and controller
    
    * Use controller templating for route
    
    * A lot of stuff
    
    * A lot of stuff
    
    * Working
    
    * Set it all to vue 3 supported format
    
    * Fix import path
    
    * Add CORS policy
    
    * Realtime user data
    
    * Add terms to competence profile
    
    * Cleanup code and improve competence profile converter
    
    * Filter on PostedAt value
    
    * initial version of the decaying average functionality
    
    * re-adding decaying average files and logic
    
    * Decaying average calculation functionality supporting both ArchitectureLayer and Skill outcomes
    
    * Decaying average calculation functional per layer per activity and skills are independent of layers
    
    ---------
    
    Co-authored-by: jan.fojtik <jan.fojtik@indicia.nl>
    Co-authored-by: Tara <tarawillink@gmail.com>
    Co-authored-by: Jelle Maas <typiqally@gmail.com>
    Co-authored-by: Sven <svenroermond@hotmail.com>
    Co-authored-by: Neal Geilen <info@nealgeilen.nl>

---
+++ Epsilon.Abstractions/Model/DecayingAveragePerActivity.cs
@@ -0,0 +1,6 @@
+﻿namespace Epsilon.Abstractions.Model;
+
+public record DecayingAveragePerActivity(
+    int Activity,
+    double DecayingAverage
+);
--- Epsilon.Canvas.Abstractions/Model/OutcomeResultCollection.cs
+++ Epsilon.Canvas.Abstractions/Model/OutcomeResultCollection.cs
@@ -6,4 +6,20 @@ public record OutcomeResultCollection(
     [property: JsonPropertyName("outcome_results")]
     IEnumerable<OutcomeResult> OutcomeResults,
     [property: JsonPropertyName("linked")] OutcomeResultCollectionLink? Links
-);
+)
+{
+    public double GetDecayingAverage()
+    {
+        var decayingAverage = 0.0;
+        
+        foreach(var grade in OutcomeResults)
+        {
+            if (grade.Score != null)
+            {
+                decayingAverage = decayingAverage * 0.35 + grade.Score.Value * 0.65;
+            }
+        }
+
+        return decayingAverage;
+    }
+}
