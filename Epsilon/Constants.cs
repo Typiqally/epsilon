baa4a444f329cd31126b4fb981c9ea8113c9f770
Jelle Maas <typiqally@gmail.com>
2023-04-20 20:45:21 +0200

    Remove code style violation

--- Epsilon/Constants.cs
+++ Epsilon/Constants.cs
@@ -2,9 +2,6 @@ namespace Epsilon;
 
 public static class Constants
 {
-    private readonly static string AnalysisTest = "";
-    
-    
     public const string ProjectName = "Epsilon";
     public static readonly Uri RepositoryUri = new Uri("https://github.com/Typiqally/epsilon");
 }
