d7dd96fe4206433b4458e518dd37b2d262611145
Jelle Maas <typiqally@gmail.com>
2023-04-20 20:43:15 +0200

    Push code style offense to validate CI pipeline

--- Epsilon/Constants.cs
+++ Epsilon/Constants.cs
@@ -2,6 +2,9 @@ namespace Epsilon;
 
 public static class Constants
 {
+    private readonly static string AnalysisTest = "";
+    
+    
     public const string ProjectName = "Epsilon";
     public static readonly Uri RepositoryUri = new Uri("https://github.com/Typiqally/epsilon");
 }
