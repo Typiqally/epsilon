b1ebf4eb5d33ccca95c2ff3082905f9a354de4a6
Jelle Maas <typiqally@gmail.com>
2023-05-10 13:57:52 +0200

    Remove redundant interface

--- Epsilon.Abstractions/Component/IEpsilonComponentWordConverter.cs
+++
@@ -1,7 +0,0 @@
-﻿using DocumentFormat.OpenXml;
-
-namespace Epsilon.Abstractions.Component;
-
-public interface IEpsilonComponentWordConverter<in TComponent> : IEpsilonComponentConverter<OpenXmlElement, TComponent>
-{
-}
