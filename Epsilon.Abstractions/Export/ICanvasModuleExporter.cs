963fed44a38cd31ead6c8cb3de98c393f7de9a1a
Jelle Maas <typiqally@gmail.com>
2023-05-10 12:58:40 +0200

    Remove CLI projects and its dependencies

--- Epsilon.Abstractions/Export/ICanvasModuleExporter.cs
+++
@@ -1,7 +0,0 @@
-﻿using Epsilon.Abstractions.Model;
-
-namespace Epsilon.Abstractions.Export;
-
-public interface ICanvasModuleExporter : IExporter<ExportData>
-{
-}
--- Epsilon.Abstractions/Export/IModuleExporterCollection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Epsilon.Abstractions.Export;
-
-public interface IModuleExporterCollection
-{
-    public IEnumerable<string> Formats();
-
-    public IDictionary<string, ICanvasModuleExporter> DetermineExporters(IEnumerable<string> formats);
-}
