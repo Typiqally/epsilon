0d21121f67911011d3d06706a8cff7c7e04e3c74
Jelle Maas <typiqally@gmail.com>
2023-05-23 11:25:16 +0200

    Remove unused constants

--- Epsilon/Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace Epsilon;
-
-public static class Constants
-{
-    public const string ProjectName = "Epsilon";
-    public static readonly Uri RepositoryUri = new Uri("https://github.com/Typiqally/epsilon");
-}
