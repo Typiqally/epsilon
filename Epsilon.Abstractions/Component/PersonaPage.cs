c83d72c7dd6b17d0ad474e5678b0c62b922fb9dd
Jelle Maas <typiqally@gmail.com>
2023-05-23 11:45:23 +0200

    Cleanup code

--- Epsilon.Abstractions/Component/PersonaPage.cs
+++ Epsilon.Abstractions/Component/PersonaPage.cs
@@ -10,21 +10,17 @@ public record PersonaPage(string PersonaHtml) : ICompetenceWordComponent
 {
     public OpenXmlElement ToWord(MainDocumentPart mainDocumentPart)
     {
-        var body = new Body();
-
-        const string altChunkId = "PersonaPage";
-
-        var personaHtmlStream = new MemoryStream(Encoding.UTF8.GetPreamble().Concat(Encoding.UTF8.GetBytes($"<html>{PersonaHtml}</html>")).ToArray());
-        var formatImportPart = mainDocumentPart.AddAlternativeFormatImportPart(AlternativeFormatImportPartType.Html, altChunkId);
-
+        var personaHtmlBuffer = Encoding.UTF8.GetPreamble().Concat(Encoding.UTF8.GetBytes($"<html>{PersonaHtml}</html>")).ToArray();
+        var personaHtmlStream = new MemoryStream(personaHtmlBuffer);
+        
+        var formatImportPart = mainDocumentPart.AddAlternativeFormatImportPart(AlternativeFormatImportPartType.Html);
         formatImportPart.FeedData(personaHtmlStream);
-        var altChunk = new AltChunk
-        {
-            Id = altChunkId,
-        };
 
-        body.Append(altChunk);
-
-        return body;
+        return new Body(
+            new AltChunk
+            {
+                Id = mainDocumentPart.GetIdOfPart(formatImportPart),
+            }
+        );
     }
 }
