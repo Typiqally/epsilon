beb6a3eb5759137acf4209faf25c9f6e0c8d63f6
Tim <timbrouwers1@gmail.com>
2023-06-12 13:08:43 +0200

    Added page create and update functions

--- Epsilon.Canvas.Abstractions/Service/IPageHttpService.cs
+++ Epsilon.Canvas.Abstractions/Service/IPageHttpService.cs
@@ -4,6 +4,12 @@ namespace Epsilon.Canvas.Abstractions.Service;
 
 public interface IPageHttpService
 {
+    Task<Page?> UpdateOrCreatePage(int courseId, string pageName, string pageContent);
+
+    Task<Page?> CreatePage(int courseId, string pageName, string pageContent);
+
+    Task<Page?> UpdatePage(int courseId, string pageName, string pageContent);
+
     Task<string?> GetPageByName(int courseId, string pageName);
 
     Task<IEnumerable<Page>?> GetAll(int courseId, IEnumerable<string> include);
--- Epsilon.Canvas/Service/PageHttpService.cs
+++ Epsilon.Canvas/Service/PageHttpService.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
 using Epsilon.Canvas.Abstractions.Model;
 using Epsilon.Canvas.Abstractions.Service;
 using Epsilon.Canvas.Http;
@@ -13,9 +15,47 @@ public class PageHttpService : HttpService, IPageHttpService
     {
     }
 
+    public async Task<Page?> UpdateOrCreatePage(int courseId, string pageName, string pageContent)
+    {
+        var existingPage = await GetPageByName(courseId, pageName);
+
+        if (existingPage == null)
+        {
+            return await CreatePage(courseId, pageName, pageContent);
+        }
+
+        return await UpdatePage(courseId, pageName, pageContent);
+    }
+    
+    public async Task<Page?> CreatePage(int courseId, string pageName, string pageContent)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Post, $"v1/courses/{courseId}/pages");
+        var pageCreation = new { wiki_page = new { title = pageName, body = pageContent } };
+        request.Content = new StringContent(JsonSerializer.Serialize(pageCreation), Encoding.UTF8, "application/json");
+
+        using var response = await Client.SendAsync(request);
+
+        return response.StatusCode == HttpStatusCode.OK
+            ? (await response.Content.ReadFromJsonAsync<Page?>()) 
+            : null;
+    }
+
+    public async Task<Page?> UpdatePage(int courseId, string pageName, string pageContent)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Put, $"v1/courses/{courseId}/pages/{pageName}");
+        var pageUpdate = new { wiki_page = new { title = pageName, body = pageContent } };
+        request.Content = new StringContent(JsonSerializer.Serialize(pageUpdate), Encoding.UTF8, "application/json");
+
+        using var response = await Client.SendAsync(request);
+
+        return response.StatusCode == HttpStatusCode.OK
+            ? (await response.Content.ReadFromJsonAsync<Page?>())
+            : null;
+    }
+
     public async Task<string?> GetPageByName(int courseId, string pageName)
     {
-        using var request = new HttpRequestMessage(HttpMethod.Get, $"v1/courses/{courseId}/{pageName}");
+        using var request = new HttpRequestMessage(HttpMethod.Get, $"v1/courses/{courseId}/pages/{pageName}");
         using var response = await Client.SendAsync(request);
 
         return response.StatusCode == HttpStatusCode.OK
