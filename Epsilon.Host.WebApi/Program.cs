1270909c9cd3750de230ff364b5037bd1a78a5d0
Jelle Maas <typiqally@gmail.com>
2023-06-20 13:13:23 +0200

    Enforce lowercase urls for routing

--- Epsilon.Host.WebApi/Program.cs
+++ Epsilon.Host.WebApi/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
+builder.Services.AddRouting(static options => options.LowercaseUrls = true);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
