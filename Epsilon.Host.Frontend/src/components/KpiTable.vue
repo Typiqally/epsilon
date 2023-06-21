91858c36d6b51144ec647e1897de3ef96d05ae35
Sven Hansen <76601644+SyntaxSven@users.noreply.github.com>
2023-06-21 21:56:37 +0200

    Cleanup kpi table component

--- Epsilon.Host.Frontend/src/components/KpiTable.vue
+++ Epsilon.Host.Frontend/src/components/KpiTable.vue
@@ -7,52 +7,46 @@ const today = new Date()
 const otherMonth = new Date()
 otherMonth.setMonth(otherMonth.getMonth() - 3)
 
-const data: Ref<unknown | undefined> = ref(undefined)
-const Kpis: Ref<unknown | undefined> = ref(undefined)
+const data: Ref<unknown> = ref<unknown>()
+const Kpis: Ref<unknown> = ref<unknown>([])
 
 api.component
     .componentDetail("kpi_table", {
-        startDate:
-            otherMonth.getFullYear() +
-            "-" +
-            `${otherMonth.getMonth() + 1}`.padStart(2, "0") +
-            "-" +
-            otherMonth.getDate(),
-        endDate:
-            today.getFullYear() +
-            "-" +
-            `${today.getMonth() + 1}`.padStart(2, "0") +
-            "-" +
-            today.getDate(),
+        startDate: formatDate(otherMonth),
+        endDate: formatDate(today),
     })
-    .then(async (result) => (data.value = result.data))
-    .then(() => {
-        Kpis.value = []
-        data.value.entries.map((entry, index) => {
-            Kpis.value[index] = entry
-        })
+    .then((result) => {
+        data.value = result.data
+        Kpis.value = result.data.entries
     })
+
+function formatDate(date: Date): string {
+    const year = date.getFullYear()
+    const month = `${date.getMonth() + 1}`.padStart(2, "0")
+    const day = date.getDate()
+    return `${year}-${month}-${day}`
+}
 </script>
 
 <template>
-    <table v-if="!!data">
+    <table v-if="data">
         <tr>
             <th>KPI</th>
-            <th style="">Assignments</th>
+            <th>Assignments</th>
             <th>Grades</th>
         </tr>
         <tr v-for="KPI of Kpis" :key="KPI">
-            <td
-                :style="{
-                    border: '3px solid #' + KPI.level.color,
-                }">
+            <td :style="{
+                border: '3px solid #' + KPI.level.color,
+            }"> 
                 {{ KPI.kpi }}
             </td>
             <td>
                 <div
                     v-for="assignment of KPI.assignments"
-                    :style="{ textAlign: 'start' }">
-                    <a :href="`${assignment.link}`">{{ assignment.name }}</a>
+                    :style="{ textAlign: 'start' }"
+                >
+                    <a :href="assignment.link">{{ assignment.name }}</a>
                 </div>
             </td>
             <td>
@@ -71,12 +65,3 @@ th {
     padding: 1rem;
 }
 </style>
-
-<!-- 
-:key="entry"   
-:style="{
-    'background-color':
-        '#' +
-        entry.outcomes.find((o) => o.id === KPI.id)
-            ?.gradeStatus.color,
-}" -->
