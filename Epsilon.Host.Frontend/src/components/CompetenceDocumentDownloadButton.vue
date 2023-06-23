377a684776ed15bc764d83c97b5e387797056ed0
WouterPennings <wouterpennings@gmail.com>
2023-06-23 11:29:12 +0200

    lint fixes, removed image

--- Epsilon.Host.Frontend/src/components/CompetenceDocumentDownloadButton.vue
+++ Epsilon.Host.Frontend/src/components/CompetenceDocumentDownloadButton.vue
@@ -1,81 +1,84 @@
 <template>
     <a @click="downloadCompetenceDocument()">
-      <template v-if="!isDownloading">
-        <span class="icon-wrapper">
-          <font-awesome-icon icon="file-arrow-down" size="lg" alt="download-button-icon" />
-        </span>
-      </template>
-      <template v-else>
-        <RoundLoader class="loading-icon"/>
-      </template>
-      Download document
+        <template v-if="!isDownloading">
+            <span class="icon-wrapper">
+                <font-awesome-icon
+                    icon="file-arrow-down"
+                    size="lg"
+                    alt="download-button-icon" />
+            </span>
+        </template>
+        <template v-else>
+            <RoundLoader class="loading-icon" />
+        </template>
+        Download document
     </a>
-  </template>
-  
-  <script lang="ts" setup>
-    import { ref } from 'vue'
-    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-    import RoundLoader from "@/components/RoundLoader.vue"
-    import { Api } from "../logic/Api"
+</template>
 
-    const api = new Api();
+<script lang="ts" setup>
+import { ref } from "vue"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import RoundLoader from "@/components/RoundLoader.vue"
+import { Api } from "../logic/Api"
 
-    const isDownloading = ref(false);
-  
-    async function downloadCompetenceDocument() {
-      // Setting downloading ref to true, triggering loading icon
-      isDownloading.value = true;
+const api = new Api()
 
-      let wordlist = await api.document.wordList();
-      let data = await wordlist.text();
-      const blob = new Blob([data], { type: 'application/octet-stream' });
+const isDownloading = ref(false)
 
-      // Create a temporary anchor element
-      const downloadLink = document.createElement("a");
-      downloadLink.href = URL.createObjectURL(blob);
-      downloadLink.download = "competence_document.docx";
+async function downloadCompetenceDocument() {
+    // Setting downloading ref to true, triggering loading icon
+    isDownloading.value = true
 
-      // Programmatically trigger the download
-      downloadLink.click();
+    const wordlist = await api.document.wordList()
+    const data = await wordlist.text()
+    const blob = new Blob([data], { type: "application/octet-stream" })
 
-      // Clean up resources
-      URL.revokeObjectURL(downloadLink.href);
+    // Create a temporary anchor element
+    const downloadLink = document.createElement("a")
+    downloadLink.href = URL.createObjectURL(blob)
+    downloadLink.download = "competence_document.docx"
 
-      // Setting downloading ref to false, triggering regular icon
-      isDownloading.value = false;
-    }
-  </script>
-  
+    // Programmatically trigger the download
+    downloadLink.click()
+
+    // Clean up resources
+    URL.revokeObjectURL(downloadLink.href)
+
+    // Setting downloading ref to false, triggering regular icon
+    isDownloading.value = false
+}
+</script>
+
 <style lang="scss" scoped>
 $icon-size: 40px;
 
 a {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 200px;
-  color: black;
-  background-color: #f2f3f8;
-  text-align: center;
-  padding: 1rem;
-  border-radius: 9px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 200px;
+    color: black;
+    background-color: #f2f3f8;
+    text-align: center;
+    padding: 1rem;
+    border-radius: 9px;
 }
 
 a:hover {
-  color: white;
-  background-color: #848da4;
+    color: white;
+    background-color: #848da4;
 }
 
 .icon-wrapper {
-  width: $icon-size;
-  height: $icon-size;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: $icon-size;
+    height: $icon-size;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .loading-icon {
-  width: $icon-size;
-  height: $icon-size;
+    width: $icon-size;
+    height: $icon-size;
 }
 </style>
--- Epsilon.Host.Frontend/src/main.ts
+++ Epsilon.Host.Frontend/src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from "vue"
 import router from "./router"
 import "./style.scss"
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons"
 import App from "./App.vue"
 
 library.add(faFileArrowDown)
 
 const app = createApp(App).use(router).mount("#app")
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.mount('#app')
+app.component("FontAwesomeIcon", FontAwesomeIcon)
+app.mount("#app")
