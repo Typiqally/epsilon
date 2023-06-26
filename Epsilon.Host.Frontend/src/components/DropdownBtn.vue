9d0dd215277f49495176da45b174e9e5fa102435
Eline <elinevooijs@outlook.com>
2023-06-26 14:18:06 +0200

    Changed term select dropdown type

--- Epsilon.Host.Frontend/src/components/DropdownBtn.vue
+++ Epsilon.Host.Frontend/src/components/DropdownBtn.vue
@@ -1,11 +1,11 @@
 <template>
-    <div><img src="../assets/vue.svg" alt="" class="student-image"/></div>
-    <div class="profileselect combobox-wrapper">
+    <div><img src="../assets/vue.svg" alt="" class="student-image" /></div>
+    <div class="combobox-wrapper">
         <Combobox
             v-if="modelValue"
             :model-value="props.modelValue"
             @update:model-value="$emit('update:modelValue', $event)">
-            <div class="dropdown">
+            <div class="profileselect dropdown">
                 <ComboboxInput
                     class="dropdown-input"
                     :display-value="(modelValue) => modelValue.name"
@@ -38,44 +38,33 @@
             </ComboboxOptions>
         </Combobox>
     </div>
-    <div class="termselect combobox-wrapper">
-        <Combobox
-            v-if="modelValue"
-            :model-value="props.modelValue"
-            @update:model-value="$emit('update:modelValue', $event)">
-            <div class="dropdown">
-                <ComboboxInput
-                    class="dropdown-input"
-                    :display-value="(modelValue) => modelValue.name"
-                    @update:model-value="$emit('update:modelValue', $event)" />
-                <ComboboxButton class="list-arrow">
-                    <ChevronUpDownIcon aria-hidden="true" />
-                </ComboboxButton>
-            </div>
-            <ComboboxOptions class="dropdown-options">
-                <div v-if="filteredTerm.length === 0 && query !== ''">
-                    Nothing found.
-                </div>
-
-                <ComboboxOption
-                    v-for="item in items"
-                    v-slot="{ active, selected }"
-                    :key="item.name"
-                    class="dropdown-option"
-                    :value="item"
-                    as="template">
-                    <li>
-                        <span>
-                            {{ item.name }}
-                        </span>
-                        <span v-if="selected">
-                            <CheckIcon aria-hidden="true" />
-                        </span>
-                    </li>
-                </ComboboxOption>
-            </ComboboxOptions>
-        </Combobox>
-    </div>
+    <Listbox
+        v-if="modelValue"
+        :model-value="props.modelValue"
+        @update:model-value="$emit('update:modelValue', $event)">
+        <ListboxButton class="termselect dropdown">
+            <span> {{ modelValue.name }}</span>
+            <span class="list-arrow">
+                <ChevronUpDownIcon aria-hidden="true" />
+            </span>
+        </ListboxButton>
+        <ListboxOptions class="dropdown-options">
+            <ListboxOption
+                v-for="item in items"
+                v-slot="{ active, selected }"
+                :key="item.name"
+                class="dropdown-option"
+                :value="item"
+                as="template">
+                <li>
+                    <span>{{ item.name }}</span>
+                    <span v-if="selected">
+                        <CheckIcon aria-hidden="true" />
+                    </span>
+                </li>
+            </ListboxOption>
+        </ListboxOptions>
+    </Listbox>
 </template>
 
 <script lang="ts" setup>
@@ -87,6 +76,10 @@ import {
     ComboboxButton,
     ComboboxOptions,
     ComboboxOption,
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
 } from "@headlessui/vue"
 import { ChevronUpDownIcon, CheckIcon } from "@heroicons/vue/20/solid"
 
@@ -112,7 +105,6 @@ const filteredTerm = computed(() =>
 </script>
 
 <style scoped>
-
 .student-image {
     width: 3rem;
     height: 3rem;
@@ -124,44 +116,50 @@ const filteredTerm = computed(() =>
     background-color: #fff;
 }
 .profileselect {
-    margin-right: 2rem;
-    min-width: 15rem;
+    grid-template-columns: 1fr 60px;
 }
 
 .termselect {
     min-width: 8rem;
+    grid-template-columns: 1fr 20px;
+    gap: 1rem;
+    border-radius: 7px;
 }
 
 .combobox-wrapper {
     background-color: #fff;
     height: 3rem;
-    border-radius: 5px;
+    border-radius: 7px;
+    margin-right: 2rem;
+    min-width: 15rem;
 }
 
 .list-arrow {
     background-color: transparent;
     border: none;
     border-radius: 0;
+    align-self: flex-end;
 }
 .list-arrow:hover,
 .list-arrow:focus,
-.list-arrow:active {
+.list-arrow:active,
+.termselect {
     border: none;
     outline: none;
 }
-.list-arrow svg,
-.list-select svg {
+.list-arrow svg {
+    height: 20px;
     max-height: 30px;
 }
 
 .dropdown {
     display: grid;
-    grid-template-columns: 1fr 60px;
     align-items: center;
     border: none;
     font-weight: 400;
     text-align: left;
     min-width: 6rem;
+    height: 3rem;
     max-height: 3rem;
 }
 
@@ -172,7 +170,6 @@ const filteredTerm = computed(() =>
     height: 3rem;
     width: 6rem;
     font-size: 1rem;
-    
 }
 
 .dropdown-options {
