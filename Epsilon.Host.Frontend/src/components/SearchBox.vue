df573f001342f4a8242b6448e097c8979ee7cdcb
Eline <97437188+ElineVooijs@users.noreply.github.com>
2023-06-27 14:35:26 +0200

    Frontend dropdown fix (#111)
    * Added Header and term dropdown
    
    * Added slider styling
    
    * Created components for homepage
    
    * Replaced slider with library
    
    * Added dropdown and slider
    
    * Fixed dropdown icon
    
    * Fixed spacing in Performance dashboard
    
    * Working term selection
    
    * Updated package.json
    
    * Changed term dropdown to combobox
    
    * Added profile select UI and fixed graph height
    
    * Changed term select dropdown type
    
    * Merge conflict solves
    
    * Lint fixes
    
    * Lint fixes
    
    * Typo
    
    * Fix lint
    
    * Final touches
    
    * Fixed list error
    
    * Fixed searchbox error
    
    ---------
    
    Co-authored-by: Kalle Pronk <kallepronk@gmail.com>
    Co-authored-by: Neal Geilen <info@nealgeilen.nl>

--- Epsilon.Host.Frontend/src/components/SearchBox.vue
+++ Epsilon.Host.Frontend/src/components/SearchBox.vue
@@ -48,6 +48,7 @@ import {
     Combobox,
     ComboboxButton,
     ComboboxInput,
+    ComboboxOptions,
     ComboboxOption,
 } from "@headlessui/vue"
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid"
