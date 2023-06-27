7aef0820c3e1dbd341ff594a79c8cb8da63b2bad
Jelle Maas <typiqally@gmail.com>
2023-06-27 11:16:55 +0200

    Add accessible student endpoint (#110)

--- Epsilon.Canvas.Abstractions/Model/GraphQl/SubmissionsConnectionNode.cs
+++ Epsilon.Canvas.Abstractions/Model/GraphQl/SubmissionsConnectionNode.cs
@@ -6,6 +6,7 @@ public record SubmissionsConnectionNode(
     [property: JsonPropertyName("submittedAt")] DateTime? SubmittedAt,
     [property: JsonPropertyName("postedAt")] DateTime? PostedAt,
     [property: JsonPropertyName("assignment")] Assignment? Assignment,
+    [property: JsonPropertyName("user")] User? User,
     [property: JsonPropertyName("submissionHistoriesConnection")] SubmissionsHistoriesConnection SubmissionsHistories,
     [property: JsonPropertyName("rubricAssessmentsConnection")] RubricAssessmentsConnection RubricAssessmentsConnection
 );
